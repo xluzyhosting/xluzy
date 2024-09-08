a8e9249d035affded1124c8d79d307be7f5e7d00
XluzyModz <fahtyrrahmann@gmail.com>
2024-09-08 20:58:42 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -65,7 +65,7 @@ check_token() {
   echo -e "${YELLOW}MASUKAN AKSES TOKEN :${NC}"
   read -r USER_TOKEN
 
-  if [ "$USER_TOKEN" = "1132" ]; then
+  if [ "$USER_TOKEN" = "xluzyganteng" ]; then
     echo -e "${GREEN}AKSES BERHASIL${NC}}"
   else
     echo -e "${GREEN}Buy dulu Ke XluzyHost${NC}"
